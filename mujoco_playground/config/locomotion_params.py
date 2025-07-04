2c8cdd88f005d6e77965fce6a8e89a59f0cb507c
pupperproject <kiwibird3@gmail.com>
2025-07-05 02:18:04 +0800

    0705-11.1

--- mujoco_playground/config/locomotion_params.py
+++ mujoco_playground/config/locomotion_params.py
@@ -317,7 +317,7 @@ def brax_sac_config(env_name: str) -> config_dict.ConfigDict:
     # 這些任務相對複雜，需要更多的訓練和更大的網路容量
     rl_config.num_timesteps = 10_000_000
     rl_config.grad_updates_per_step = 64 # 增加數據利用率
-    rl_config.reward_scaling = 50.0 # 增強獎勵信號
+    rl_config.reward_scaling = 30.0 # 增強獎勵信號
     rl_config.network_factory = config_dict.create(
         hidden_layer_sizes=(512, 256, 128), # 更大容量的網路
         # 如果需要，這裡可以指定 obs_key

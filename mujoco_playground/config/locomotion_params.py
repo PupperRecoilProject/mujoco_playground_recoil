9545e7135bd1f17569021bbd23e6af591fa039fe
pupperproject <kiwibird3@gmail.com>
2025-07-04 18:48:09 +0800

    0704-7

--- mujoco_playground/config/locomotion_params.py
+++ mujoco_playground/config/locomotion_params.py
@@ -283,7 +283,7 @@ def brax_sac_config(env_name: str) -> config_dict.ConfigDict:
       num_evals=50,  # 評估頻率
       episode_length=env_config.episode_length,
       action_repeat=1,
-      num_envs=256,  # SAC 通常使用多個並行環境來收集數據到 Replay Buffer
+      num_envs=8192,  # SAC 通常使用多個並行環境來收集數據到 Replay Buffer
       num_eval_envs=128,
       seed=0,
 
@@ -315,7 +315,7 @@ def brax_sac_config(env_name: str) -> config_dict.ConfigDict:
   # 這裡只針對我們新創建的 PupperJoystickSac 環境進行配置
   if env_name in ("PupperJoystickSacFlatTerrain", "PupperJoystickSacRoughTerrain"):
     # 這些任務相對複雜，需要更多的訓練和更大的網路容量
-    rl_config.num_timesteps = 1_000_000
+    rl_config.num_timesteps = 100_000_000
     rl_config.grad_updates_per_step = 1 # 增加數據利用率
     rl_config.reward_scaling = 10.0 # 增強獎勵信號
     rl_config.network_factory = config_dict.create(

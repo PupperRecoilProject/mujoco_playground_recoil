6335a6d71ad1c496dc62c62afe7e1ace7b61c7df
pupperproject <kiwibird3@gmail.com>
2025-07-05 00:29:10 +0800

    0704-9.1

--- mujoco_playground/config/locomotion_params.py
+++ mujoco_playground/config/locomotion_params.py
@@ -290,7 +290,7 @@ def brax_sac_config(env_name: str) -> config_dict.ConfigDict:
       # Replay Buffer 參數
       min_replay_size=16_384, # 學習開始前的探索步數
       max_replay_size=1_048_576,
-      batch_size=128,
+      batch_size=512,
       grad_updates_per_step=64, # 每收集一步數據，進行一次網路更新
 
       # SAC 演算法核心參數
@@ -315,9 +315,9 @@ def brax_sac_config(env_name: str) -> config_dict.ConfigDict:
   # 這裡只針對我們新創建的 PupperJoystickSac 環境進行配置
   if env_name in ("PupperJoystickSacFlatTerrain", "PupperJoystickSacRoughTerrain"):
     # 這些任務相對複雜，需要更多的訓練和更大的網路容量
-    rl_config.num_timesteps = 16_777_216
+    rl_config.num_timesteps = 1_000_000
     rl_config.grad_updates_per_step = 64 # 增加數據利用率
-    rl_config.reward_scaling = 5.0 # 增強獎勵信號
+    rl_config.reward_scaling = 30.0 # 增強獎勵信號
     rl_config.network_factory = config_dict.create(
         hidden_layer_sizes=(512, 256, 128), # 更大容量的網路
         # 如果需要，這裡可以指定 obs_key
